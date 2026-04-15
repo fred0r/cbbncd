f766302dfe613b2372b50a5b9a3a9a815772481d
fred <fred@home>
2026-04-15 04:51:35 +0200

    cbbncd-0.25 original source

---
+++ src/bnc/bnc.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <list>
+#include <string>
+#include <vector>
+
+#include "../core/eventreceiver.h"
+#include "../core/types.h"
+
+#include "../address.h"
+
+class BncSession;
+
+class Bnc : private Core::EventReceiver {
+public:
+  Bnc(int listenport, const std::list<Address>& siteaddrs, bool ident, bool noidnt, bool traffic, bool nat, const std::list<Address>& natips, int pasvportfirst, int pasvportlast);
+private:
+  void FDNew(int sockid, int newsockid) override;
+  void FDFail(int sockid, const std::string& error) override;
+  std::list<BncSession*> sessions;
+  int listenport;
+  std::vector<Address> siteaddrs;
+  std::string host;
+  bool ident;
+  bool noidnt;
+  bool traffic;
+  bool nat;
+  std::vector<Address> natips;
+  int nextsiteaddr;
+};
